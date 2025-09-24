29815d2af338c59436216aea4710b8302cd473f9
Albin Carlsson <github.garter593@passmail.net>
2025-09-24 13:20:40 +0200

    sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,6 +1,5 @@
 import * as sitemap from 'super-sitemap';
 import type { RequestHandler } from '@sveltejs/kit';
-import { env } from '$env/dynamic/public';
 import { categories } from '$lib/config/components.svelte';
 
 export const GET: RequestHandler = async () => {
@@ -9,7 +8,7 @@ export const GET: RequestHandler = async () => {
 		.map((category) => category.slug);
 
 	return await sitemap.response({
-		origin: env.PUBLIC_APP_URL,
+		origin: 'https://shadcnstudio-svelte.netlify.app',
 		paramValues: {
 			'/docs/components/[category]': categorySlugs
 		},
