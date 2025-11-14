82b20d12aadf206ea94488f0c4efa6801ef5ae94
Albin Carlsson <github.garter593@passmail.net>
2025-11-14 20:08:14 +0100

    rm 3 docs

--- src/lib/utils/pages.ts
+++ src/lib/utils/pages.ts
@@ -6,8 +6,8 @@ export const gettingStartedPages = [
 		slug: 'how-to-use-shadcn-cli',
 		title: 'How to use Shadcn CLI',
 		href: `${GETTING_STARTED_HREF}/how-to-use-shadcn-cli`
-	},
-	{ slug: 'components', title: 'Components', href: `${GETTING_STARTED_HREF}/components` },
-	{ slug: 'blocks', title: 'Blocks', href: `${GETTING_STARTED_HREF}/blocks` },
-	{ slug: 'copy-prompt', title: 'Copy Prompt', href: `${GETTING_STARTED_HREF}/copy-prompt` }
+	}
+	// { slug: 'components', title: 'Components', href: `${GETTING_STARTED_HREF}/components` },
+	// { slug: 'blocks', title: 'Blocks', href: `${GETTING_STARTED_HREF}/blocks` },
+	// { slug: 'copy-prompt', title: 'Copy Prompt', href: `${GETTING_STARTED_HREF}/copy-prompt` }
 ];
