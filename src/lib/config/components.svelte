69edd57d9e78f3740662117a73769f7d77062c19
Albin Carlsson <github.garter593@passmail.net>
2025-12-21 20:03:51 +0100

    motion-tooltip

--- src/lib/config/components.svelte
+++ src/lib/config/components.svelte
@@ -1242,7 +1242,7 @@
 				{ name: 'tooltip-13' },
 				{ name: 'tooltip-14' },
 				{ name: 'tooltip-15' },
-				{ name: 'tooltip-16', isAnimated: true, underConstruction: true },
+				{ name: 'tooltip-16', isAnimated: true },
 				{ name: 'tooltip-17', isAnimated: true, underConstruction: true }
 			]
 		},
---
+++ src/lib/registry/ui/motion-tooltip/index.ts
@@ -0,0 +1,11 @@
+import Root, { type MotionTooltipProps, type MotionTooltipItem } from './motion-tooltip.svelte';
+
+export {
+	Root,
+	type MotionTooltipProps as Props,
+	type MotionTooltipItem,
+	//
+	Root as MotionTooltip,
+	Root as AnimatedTooltip,
+	type MotionTooltipProps
+};
