221f5a973f04e7374604b306fad3eb934210f4f0
Albin Carlsson <github.garter593@passmail.net>
2026-01-07 14:48:00 +0100

    grow-button

---
+++ src/lib/registry/ui/grow-button/index.ts
@@ -0,0 +1,12 @@
+import PrimaryGrowButton, { type PrimaryGrowButtonProps } from './primary-grow-button.svelte';
+import SecondaryGrowButton, { type SecondaryGrowButtonProps } from './secondary-grow-button.svelte';
+
+export {
+	PrimaryGrowButton,
+	SecondaryGrowButton,
+	type PrimaryGrowButtonProps as PrimaryProps,
+	type SecondaryGrowButtonProps as SecondaryProps,
+	//
+	type PrimaryGrowButtonProps,
+	type SecondaryGrowButtonProps
+};
---
+++ src/lib/registry/ui/grow-button/primary-grow-button.svelte
@@ -0,0 +1,37 @@
+<script lang="ts" module>
+	import { cn } from '$lib/utils';
+	import { Button, type ButtonProps } from '$lib/components/ui/button';
+
+	export type PrimaryGrowButtonProps = ButtonProps;
+</script>
+
+<script lang="ts">
+	let {
+		children,
+		class: className,
+		variant = 'default',
+		size = 'default',
+		ref = $bindable(null),
+		...props
+	}: PrimaryGrowButtonProps = $props();
+</script>
+
+<Button
+	bind:ref
+	{variant}
+	{size}
+	class={cn(
+		'rounded-lg duration-200 ease-in-out active:-translate-x-0.5 active:translate-y-0.5',
+		// default shadows
+		'[box-shadow:0px_1px_8px_0px_color-mix(in_oklab,white_7%,transparent)_inset,0px_0px_4.3px_0px_color-mix(in_oklab,var(--primary)_11%,transparent)_inset,0px_0px_0px_2.5px_var(--primary),0px_9px_21.7px_3px_color-mix(in_oklab,var(--primary)_40%,transparent),0px_-1px_0px_1px_color-mix(in_oklab,white_18%,transparent)_inset,0px_4px_4px_0px_color-mix(in_oklab,var(--primary)_16%,transparent)] dark:[box-shadow:0px_1px_8px_0px_color-mix(in_oklab,black_7%,transparent)_inset,0px_0px_4.3px_0px_color-mix(in_oklab,var(--primary)_11%,transparent)_inset,0px_0px_0px_2.5px_var(--primary),0px_9px_21.7px_3px_color-mix(in_oklab,var(--primary)_40%,transparent),0px_-1px_0px_1px_color-mix(in_oklab,black_18%,transparent)_inset,0px_4px_4px_0px_color-mix(in_oklab,var(--primary)_16%,transparent)]',
+		// shadows on active state
+		'active:[box-shadow:0px_1px_8px_0px_color-mix(in_oklab,white_7%,transparent)_inset,0px_0px_4.3px_0px_color-mix(in_oklab,var(--primary)_11%,transparent)_inset,0px_0px_0px_2.5px_var(--primary),0px_7px_12px_0px_color-mix(in_oklab,var(--primary)_60%,transparent),0px_-1px_0px_1px_color-mix(in_oklab,white_18%,transparent)_inset,0px_4px_4px_0px_color-mix(in_oklab,var(--primary)_16%,transparent)] dark:active:[box-shadow:0px_1px_8px_0px_color-mix(in_oklab,black_7%,transparent)_inset,0px_0px_4.3px_0px_color-mix(in_oklab,var(--primary)_11%,transparent)_inset,0px_0px_0px_2.5px_var(--primary),0px_7px_12px_0px_color-mix(in_oklab,var(--primary)_60%,transparent),0px_-1px_0px_1px_color-mix(in_oklab,black_18%,transparent)_inset,0px_4px_4px_0px_color-mix(in_oklab,var(--primary)_16%,transparent)]',
+		// shadows on focus state
+		'focus-visible:[box-shadow:0px_1px_8px_0px_color-mix(in_oklab,white_7%,transparent)_inset,0px_0px_4.3px_0px_color-mix(in_oklab,var(--primary)_11%,transparent)_inset,0px_0px_0px_2.5px_var(--primary),0px_7px_12px_0px_color-mix(in_oklab,var(--primary)_60%,transparent),0px_-1px_0px_1px_color-mix(in_oklab,white_18%,transparent)_inset,0px_4px_4px_0px_color-mix(in_oklab,var(--primary)_16%,transparent)] dark:focus-visible:[box-shadow:0px_1px_8px_0px_color-mix(in_oklab,black_7%,transparent)_inset,0px_0px_4.3px_0px_color-mix(in_oklab,var(--primary)_11%,transparent)_inset,0px_0px_0px_2.5px_var(--primary),0px_7px_12px_0px_color-mix(in_oklab,var(--primary)_60%,transparent),0px_-1px_0px_1px_color-mix(in_oklab,black_18%,transparent)_inset,0px_4px_4px_0px_color-mix(in_oklab,var(--primary)_16%,transparent)]',
+		size === 'lg' && 'text-base has-[>svg]:px-6',
+		className
+	)}
+	{...props}
+>
+	{@render children?.()}
+</Button>
