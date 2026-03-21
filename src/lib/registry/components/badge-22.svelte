4a26626129aa7937fd1b93d53c4f5cdf703c5a80
Albin Carlsson <github.garter593@passmail.net>
2026-03-21 10:56:02 +0100

    use avatar instead of image badge-22

--- src/lib/registry/components/badge-22.svelte
+++ src/lib/registry/components/badge-22.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
 	import { Badge } from '$lib/components/ui/badge';
 </script>
 
-<Badge variant="outline" class="rounded-full p-1 pe-2">
-	<img src="/img/avatars/05.webp" alt="Hallie Richards" class="size-6 rounded-full" />
+<Badge variant="outline" class="h-auto rounded-full py-1 ps-1 pe-2">
+	<Avatar.Root class="size-6">
+		<Avatar.Image src="/img/avatars/05.webp" alt="Hallie Richards" />
+		<Avatar.Fallback>HR</Avatar.Fallback>
+	</Avatar.Root>
 	Avatar
 </Badge>
