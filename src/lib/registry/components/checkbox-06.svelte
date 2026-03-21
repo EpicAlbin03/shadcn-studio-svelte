9e351cafaa401627bfd9f7cf87ffb0df211e6fc8
Albin Carlsson <github.garter593@passmail.net>
2026-03-21 11:51:12 +0100

    checkbox-06 fix padding

--- src/lib/registry/components/checkbox-06.svelte
+++ src/lib/registry/components/checkbox-06.svelte
@@ -9,7 +9,7 @@
 
 <div class="flex items-center gap-2">
 	{#each snacks as label (label)}
-		<Badge variant="secondary" class="relative gap-2 px-3 py-1.5">
+		<Badge variant="secondary" class="relative gap-2 px-3 py-3.5">
 			<Checkbox
 				id={label}
 				checked={selected.includes(label)}
