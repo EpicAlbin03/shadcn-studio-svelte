52ebe72b2e9411f3bc7dce6f20c5712e8e87a123
Albin Carlsson <github.garter593@passmail.net>
2026-03-22 10:08:04 +0100

    switch 12,13,14

--- src/lib/registry/components/switch-12.svelte
+++ src/lib/registry/components/switch-12.svelte
@@ -10,16 +10,16 @@
 	<div class="relative inline-grid h-7 grid-cols-[1fr_1fr] items-center text-sm font-medium">
 		<Switch
 			bind:checked
-			class="peer absolute inset-0 h-[inherit] w-14 data-[state=checked]:bg-input/50 data-[state=unchecked]:bg-input/50 [&_span]:size-6.5 [&_span]:bg-background! [&_span]:transition-transform [&_span]:duration-300 [&_span]:ease-[cubic-bezier(0.16,1,0.3,1)] [&_span]:data-[state=checked]:translate-x-7 [&_span]:data-[state=checked]:rtl:-translate-x-7"
+			class="peer absolute inset-0 data-[size=default]:h-[inherit] data-[size=default]:w-14 data-checked:bg-input/50 data-unchecked:bg-input/50 [&_span]:bg-background! [&_span]:transition-transform [&_span]:duration-300 [&_span]:ease-[cubic-bezier(0.16,1,0.3,1)] data-[size=default]:[&_span]:size-6.5 data-[size=default]:[&_span]:data-checked:translate-x-7 rtl:data-[size=default]:[&_span]:data-checked:-translate-x-7"
 			aria-label="Switch with icon indicators"
 		/>
 		<span
-			class="pointer-events-none relative ms-1.75 flex min-w-7 items-center text-center peer-data-[state=checked]:text-muted-foreground/70"
+			class="pointer-events-none relative ms-1.75 flex min-w-7 items-center text-center peer-data-checked:text-muted-foreground/70"
 		>
 			<CheckIcon class="size-4" aria-hidden="true" />
 		</span>
 		<span
-			class="pointer-events-none relative -ms-px flex min-w-7 items-center text-center peer-data-[state=unchecked]:text-muted-foreground/70"
+			class="pointer-events-none relative -ms-px flex min-w-7 items-center text-center peer-data-unchecked:text-muted-foreground/70"
 		>
 			<XIcon class="size-4" aria-hidden="true" />
 		</span>
--- src/lib/registry/components/switch-14.svelte
+++ src/lib/registry/components/switch-14.svelte
@@ -8,16 +8,16 @@
 	<div class="relative inline-grid h-8 grid-cols-[1fr_1fr] items-center text-sm font-medium">
 		<Switch
 			bind:checked
-			class="peer absolute inset-0 h-[inherit] w-auto rounded-md data-[state=unchecked]:bg-input/50 [&_span]:z-10 [&_span]:h-full [&_span]:w-1/2 [&_span]:rounded-sm [&_span]:transition-transform [&_span]:duration-300 [&_span]:ease-[cubic-bezier(0.16,1,0.3,1)] [&_span]:data-[state=checked]:translate-x-8.75 [&_span]:data-[state=checked]:rtl:-translate-x-8.75"
+			class="peer absolute inset-0 rounded-md data-[size=default]:h-[inherit] data-[size=default]:w-auto data-unchecked:bg-input/50 [&_span]:z-10 [&_span]:rounded-sm [&_span]:transition-transform [&_span]:duration-300 [&_span]:ease-[cubic-bezier(0.16,1,0.3,1)] data-[size=default]:[&_span]:h-full data-[size=default]:[&_span]:w-1/2 data-[size=default]:[&_span]:data-checked:translate-x-9 rtl:data-[size=default]:[&_span]:data-checked:-translate-x-9"
 			aria-label="Square switch with permanent text indicators"
 		/>
 		<span
-			class="pointer-events-none relative ms-0.5 flex items-center justify-center px-2 text-center transition-transform duration-300 ease-[cubic-bezier(0.16,1,0.3,1)] peer-data-[state=checked]:invisible peer-data-[state=unchecked]:translate-x-full peer-data-[state=unchecked]:rtl:-translate-x-full"
+			class="pointer-events-none relative ms-0.5 flex items-center justify-center px-2 text-center transition-transform duration-300 ease-[cubic-bezier(0.16,1,0.3,1)] peer-data-checked:invisible peer-data-unchecked:translate-x-full peer-data-unchecked:rtl:-translate-x-full"
 		>
 			<span class="text-[10px] font-medium uppercase">No</span>
 		</span>
 		<span
-			class="pointer-events-none relative me-0.5 flex items-center justify-center px-2 text-center transition-transform duration-300 ease-[cubic-bezier(0.16,1,0.3,1)] peer-data-[state=checked]:-translate-x-full peer-data-[state=checked]:text-background peer-data-[state=unchecked]:invisible peer-data-[state=checked]:rtl:translate-x-full"
+			class="pointer-events-none relative me-0.5 flex items-center justify-center px-2 text-center transition-transform duration-300 ease-[cubic-bezier(0.16,1,0.3,1)] peer-data-checked:-translate-x-full peer-data-checked:text-background peer-data-unchecked:invisible peer-data-checked:rtl:translate-x-full"
 		>
 			<span class="text-[10px] font-medium uppercase">Yes</span>
 		</span>
